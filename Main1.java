1ca5cc2808796bcf41f08f2a81f69565ad043a42
vijay110311 <sundarvijay54321@gmail.com>
2025-01-23 23:13:54 +0530

    modified file

--- Main1.java
+++ Main1.java
@@ -1,3 +1,8 @@
+//This program is create to the  area/perimeter of calculation and how to use the object value pass in main method to another class method access.
+//I use the three class one class is main and another two is area /perimeter class.
+//Main class create two object and call or access the another class method
+//area class is used to three method/funtion square,rect,circle.
+//perimeter class is used to  square,rect,circle method.
 class area{
     double pi=3.14;
     int r;
